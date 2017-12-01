69013af5e6f503a45330ffe4427dbe7cf063d05e
leon in 't Veld <leon3110l@gmail.com>
2017-12-01 22:16:42 +0100

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,23 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
+const htmlmin = require('gulp-htmlmin');
+const browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "dist/"
+      }
+  });
+});
 
 gulp.task('js', () => {
   gulp.src('src/js/drag-and-drop.js')
     .pipe(babel())
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream())
 })
 
 gulp.task('sass', () => {
@@ -16,9 +27,18 @@ gulp.task('sass', () => {
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
 })
 
-gulp.task('watch', ["sass", "js"], () => {
+gulp.task('html', () => {
+  gulp.src("src/index.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('watch', ["sass", "js", "html", "browser-sync"], () => {
   gulp.watch("src/css/drag-and-drop.sass", ["sass"])
   gulp.watch("src/js/drag-and-drop.js", ["js"])
+  gulp.watch("src/index.html", ["html"])
 });
